d671a4d6e742e4dce670bbef72719090740e8cc6
ExtraUk <pablo.lignelli@hotmail.com>
2021-11-04 17:02:36 -0300

    Estructura Principal

---
+++ src/test/java/edu/ctc/obligatorio2/Obligatorio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ctc.obligatorio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obligatorio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
