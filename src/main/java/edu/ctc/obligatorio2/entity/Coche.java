fef2664ba0b63e1a6e56d2b5fde94cc6de3b5bcf
Zellon19 <axel.alvarez2k4@outlook.es>
2021-11-27 18:50:55 -0300

    enum coches v2

--- src/main/java/edu/ctc/obligatorio2/entity/Coche.java
+++ src/main/java/edu/ctc/obligatorio2/entity/Coche.java
@@ -3,7 +3,19 @@ package edu.ctc.obligatorio2.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Table;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
 
 @Entity
 @Table(name = "coches")
@@ -18,9 +30,10 @@ public class Coche {
 	@Column (length=7, nullable = false, unique = true)
 	private String matricula;
 	
-	@Column (nullable = false)
+	@Enumerated(EnumType.STRING)
 	private TipoCoche tipoCoche;
 
+	
 	public TipoCoche getTipoCoche() {
 		return tipoCoche;
 	}
