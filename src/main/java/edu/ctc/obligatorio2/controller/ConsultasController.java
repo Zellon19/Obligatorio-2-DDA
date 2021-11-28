60480207594021f1fe84d7ac94b8f96a74ab9fd6
ExtraUk <pablo.lignelli@hotmail.com>
2021-11-28 13:03:34 -0300

    Consultas 2 y 3 Back-End

--- src/main/java/edu/ctc/obligatorio2/controller/ConsultasController.java
+++ src/main/java/edu/ctc/obligatorio2/controller/ConsultasController.java
@@ -1,6 +1,7 @@
 package edu.ctc.obligatorio2.controller;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -46,12 +47,12 @@ public class ConsultasController {
     
     //Consulta 2
     @GetMapping({"/consulta2"})
-    public String viajesPorChoferPorFecha(@RequestParam(value="idChofer",required=true) Long id, @RequestParam(value="fecha",required=true) LocalDate fecha, Model modelo){
+    public String viajesPorChoferPorFecha(@RequestParam(value="idChofer",required=true) Long id, @RequestParam(value="fecha",required=true) LocalDateTime fecha, Model modelo){
         List<Viaje> todosLosViajes = viajeServicio.findAllViajes();
         List<Viaje> retorno = todosLosViajes;
         retorno.clear();
         for(Viaje viaje: todosLosViajes) {
-        	if(viaje.getChofer().getId() == id && viaje.getFechaHora().toLocalDate().equals(fecha)) {
+        	if(viaje.getChofer().getId() == id && viaje.getFechaHora().toLocalDate().equals(fecha.toLocalDate())) {
         		retorno.add(viaje);
         	}
         }
@@ -59,6 +60,58 @@ public class ConsultasController {
         return "consulta2.html";
     }
 	
-	
+    @GetMapping({"/consulta2"})
+    public String viajesPorChoferPorFecha(@RequestParam(value="idChofer",required=true) Long id, @RequestParam(value="fecha1",required=true) LocalDateTime fecha1, @RequestParam(value="fecha2",required=true) LocalDateTime fecha2, Model modelo){
+        List<Viaje> todosLosViajes = viajeServicio.findAllViajes();
+        List<Viaje> retorno = todosLosViajes;
+        retorno.clear();
+        for(Viaje viaje: todosLosViajes) {
+        	if(viaje.getChofer().getId() == id && (viaje.getFechaHora().toLocalDate().isAfter(fecha1.toLocalDate()) || viaje.getFechaHora().toLocalDate().isBefore(fecha2.toLocalDate()))) {
+        		retorno.add(viaje);
+        	}
+        }
+        modelo.addAttribute("consultas", retorno);
+        return "consulta2.html";
+    }
+    
+    //Consulta 3
+    @GetMapping({"/consulta3"})
+    public String recaudadoPorChoferPorViaje(@RequestParam(value="idChofer",required=true) Long idChofer, @RequestParam(value="idViaje",required=true) Long idViaje, Model modelo){
+        List<Viaje> todosLosViajes = viajeServicio.findAllViajes();
+        Float total = 0f;
+        for(Viaje viaje: todosLosViajes) {
+        	if(viaje.getChofer().getId() == idChofer && viaje.getId() == idViaje) {
+        		total += viaje.getPrecio();
+        	}
+        }
+        modelo.addAttribute("consultas", total);
+        return "consulta3.html";
+    }
+    
+    @GetMapping({"/consulta3"})
+    public String recaudadoPorChoferPorTurno(@RequestParam(value="idChofer",required=true) Long idChofer, @RequestParam(value="idTurno",required=true) Long idTurno, Model modelo){
+        List<Viaje> todosLosViajes = viajeServicio.findAllViajes();
+        Float total = 0f;
+        for(Viaje viaje: todosLosViajes) {
+        	if(viaje.getChofer().getId() == idChofer && viaje.getTurno().getId() == idTurno) {
+        		total += viaje.getPrecio();
+        	}
+        }
+        modelo.addAttribute("consultas", total);
+        return "consulta3.html";
+    }
+    
+    @GetMapping({"/consulta3"})
+    public String recaudadoPorChoferPorFecha(@RequestParam(value="idChofer",required=true) Long idChofer, @RequestParam(value="fecha",required=true) LocalDateTime fecha, Model modelo){
+        List<Viaje> todosLosViajes = viajeServicio.findAllViajes();
+        Float total = 0f;
+        for(Viaje viaje: todosLosViajes) {
+        	if(viaje.getChofer().getId() == idChofer && viaje.getFechaHora().toLocalDate().equals(fecha.toLocalDate())) {
+        		total += viaje.getPrecio();
+        	}
+        }
+        modelo.addAttribute("consultas", total);
+        return "consulta3.html";
+    }
 
 }
